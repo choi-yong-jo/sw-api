b578f894f06017528621e1fe5db1559d64aab7fd
DragonBird <cyjo1207@gmail.com>
2024-08-06 09:01:43 +0900

    CORS 설정

--- src/main/java/com/sungwon/api/config/WebMvcConfig.java
+++ src/main/java/com/sungwon/api/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.sungwon.api.config;
 import com.sungwon.api.common.interceptor.PerformanceInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,5 +19,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //                .addPathPatterns("/**")
                 .addPathPatterns("/rest-api-root/**")
                 .excludePathPatterns("/css/**", "/js/**", "/image/**");     // 인터셉터 적용 대상
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:8002")
+                .allowCredentials(true);
     }
 }
